c619effa94f41a97039e64ba5a380588568e0571
Marcus Weiner <marcus.weiner@gmail.com>
2019-10-27 21:55:14 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-set -e
+set -x
 
 LDFLAGS="-X github.com/kalkspace/orno-exporter/config.Version=${GITHUB_SHA} $GO_LDFLAGS"
-GO_OPTS="-ldflags \"${LDFLAGS}\""
-if [ -z "${BIN_FILE}" ]; then
-    GO_OPTS="-o \"${BIN_FILE}\" $GO_OPTS"
+GO_OPTS=""
+if [ ! -z "${BIN_FILE}" ]; then
+    GO_OPTS="-o ${BIN_FILE}"
 fi
 
-go build ${GO_OPTS} main.go
+go build ${GO_OPTS} -ldflags "${LDFLAGS}" main.go
